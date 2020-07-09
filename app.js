4787152d9a472ba90c0b63f40b4412ecc4bd1269
bepul0 <bepul.cse.ru@gmail.com>
2020-07-09 18:31:12 +0600

    Express Session Part 2

--- app.js
+++ app.js
@@ -48,40 +48,23 @@ app.use(
   })
 );
 
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
 function auth(req, res, next) {
   console.log(req.session);
 
   if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-      var err = new Error("You are not authentication!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      next(err);
-      return;
-    }
-    var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
-      .toString()
-      .split(":");
-
-    var username = auth[0];
-    var password = auth[1];
-    if (username == "admin" && password == "password") {
-     // res.cookie("user", "admin", { signed: true });
-     req.session.user='admin'
-      next();
-    } else {
-      var err = new Error("You are not athentication!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      next(err);
-    }
+    var err = new Error("You are not authentication!");
+    err.status = 401;
+    next(err);
+    return;
   } else {
-    if (req.session.user == "admin") {
+    if (req.session.user == "authenticated") {
       next();
     } else {
       var err = new Error("You are not athentication!");
-      err.status = 401;
+      err.status = 403;
       next(err);
     }
   }
@@ -90,9 +73,6 @@ function auth(req, res, next) {
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
