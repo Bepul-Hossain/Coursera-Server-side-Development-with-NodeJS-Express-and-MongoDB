653a5ccea596bd238238791704802a2c1daf06ae
bepul0 <bepul.cse.ru@gmail.com>
2020-07-16 10:13:24 +0600

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/conFusion",
+  facebook: {
+    clientId: "607587910165928",
+    clientSecret: "fd797f391a9541c32e29712011dcd2f1",
+  },
 };
